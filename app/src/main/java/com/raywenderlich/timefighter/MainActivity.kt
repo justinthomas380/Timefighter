3c946f6f9d0c13a072687237a7ad3993a0da65e6
Justin A Thomas <84940076+justinthomas380@users.noreply.github.com>
2024-01-29 21:15:21 -0500

    Chapter 2 and Chapter 3 completed up to page 78.

---
+++ app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.raywenderlich.timefighter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var gameScoreTextView: TextView
+    private lateinit var timeLeftTextView: TextView
+    private lateinit var tapMeButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // connect views to variables
+    }
+
+    private fun incrementScore() {
+        // increment score logic
+    }
+
+    private fun resetGame() {
+        // reset game logic
+    }
+
+    private fun startGame() {
+        // start game logic
+    }
+
+    private fun endGame(){
+        // end game logic
+    }
+}
